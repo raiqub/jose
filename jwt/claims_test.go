5555ab52f844e97638348fee56bbda1ea9a53cab
Fabrício Godoy <skarllot@gmail.com>
2016-05-23 14:15:45 -0300

    New test for jwt.CommonClaims

--- jwt/claims_test.go
+++ jwt/claims_test.go
@@ -16,6 +16,63 @@
 
 package jwt
 
-const (
-	payload = ``
+import (
+	"bytes"
+	"testing"
 )
+
+const (
+	payload = `eyJpc3MiOiJhdXRoLmV4YW1wbGUuY29tIiwiYXVkIjoiMTIzNDU2Nzg5MCIsInN1YiI6Im` +
+		`pvaG4uZG9lQGV4YW1wbGUuY29tIiwiZXhwIjoxMzAwODE5MzgwLCJ1c2VyIjp7Im5hbWUiOiJKb2` +
+		`huIERvZSIsInNjb3BlcyI6WyJhZG1pbiJdfX0`
+	reencodeResult = `eyAiaXNzIjoiYXV0aC5leGFtcGxlLmNvbSIsInN1YiI6ImpvaG4uZG9lQGV4YW1` +
+		`wbGUuY29tIiwiYXVkIjoiMTIzNDU2Nzg5MCIsImV4cCI6MTMwMDgxOTM4MCwidXNlciI6eyAibmF` +
+		`tZSI6IkpvaG4gRG9lIiwiZW1haWwiOiIiLCJzY29wZXMiOlsiYWRtaW4iXX19`
+)
+
+func TestDecodeAndEncode(t *testing.T) {
+	var claims CommonClaims
+	if err := claims.Decode(payload); err != nil {
+		t.Fatalf("Error decoding payload: %v", err)
+	}
+
+	if claims.Issuer != "auth.example.com" {
+		t.Errorf("Invalid issuer value: %s", claims.Subject)
+	}
+	if claims.Audience != "1234567890" {
+		t.Errorf("Invalid audience value: %s", claims.Audience)
+	}
+	if claims.Subject != "john.doe@example.com" {
+		t.Errorf("Invalid subject value: %s", claims.Subject)
+	}
+	if claims.ExpireAt.ToInt64() != 1300819380 {
+		t.Errorf("Invalid expiration value: %d", claims.ExpireAt.ToInt64())
+	}
+	if claims.User == nil {
+		t.Error("User field should not be null")
+	} else {
+		usr := claims.User
+		if usr.Name != "John Doe" {
+			t.Errorf("Invalid user name value: %s", usr.Name)
+		}
+		if usr.Scopes == nil {
+			t.Error("User scopes should not be null")
+		}
+		if len(usr.Scopes) != 1 {
+			t.Errorf("Unexpected user scopes length: %d", len(usr.Scopes))
+		}
+		if usr.Scopes[0] != "admin" {
+			t.Errorf("Invalid user scope value: %s", usr.Scopes[0])
+		}
+	}
+
+	var buf bytes.Buffer
+	if err := claims.Encode(&buf); err != nil {
+		t.Errorf("Error encoding claims: %v", err)
+	}
+
+	bufStr := buf.String()
+	if bufStr != reencodeResult {
+		t.Errorf("Unexpected result for claims encoding: %s", bufStr)
+	}
+}
